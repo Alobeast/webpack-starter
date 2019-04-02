eb5bd2ddf881a69d78dc0b90625a4b5165c2ff48
Alois Beard <alois.beard@gmail.com>
2019-04-02 16:55:27 +0200

    completed as much as I could

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,3 +2,9 @@ import '../styles/index.scss';
 import 'bootstrap';
 
 console.log('webpack starterkit');
+
+document.querySelectorAll(".item").forEach((item) => {
+  item.addEventListener("click", (event) => {
+    event.currentTarget.classList.toggle("img-selected");
+  });
+});
